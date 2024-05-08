b3b86f6f2d7633665b60c74ffebc204077ab96c1
Adam Kliza <36967538+eNesPL@users.noreply.github.com>
2024-05-08 22:07:12 +0200

    Update GogoAnime.kt

--- src/en/gogoanime/src/eu/kanade/tachiyomi/animeextension/en/gogoanime/GogoAnime.kt
+++ src/en/gogoanime/src/eu/kanade/tachiyomi/animeextension/en/gogoanime/GogoAnime.kt
@@ -30,7 +30,7 @@ class GogoAnime : ConfigurableAnimeSource, ParsedAnimeHttpSource() {
 
     // TODO: Check frequency of url changes to potentially
     // add back overridable baseurl preference
-    override val baseUrl = "https://ww4.gogoanime2.org"
+    override val baseUrl = "https://gogoanime2.org"
 
     override val lang = "en"
 
